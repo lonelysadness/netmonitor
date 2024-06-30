5e72c580a110255ff0ab7d5cbc32accfc05cd529
lonelysadness <lonelysadness@proton.me>
2024-06-30 16:13:24 +0200

    geoip fix

--- go.mod
+++ go.mod
@@ -6,10 +6,8 @@ toolchain go1.22.4
 
 require (
 	github.com/chifflier/nfqueue-go v0.0.0-20170228160439-61ca646babef
+	github.com/oschwald/geoip2-golang v1.11.0
 	golang.org/x/sys v0.21.0
 )
 
-require (
-	github.com/oschwald/geoip2-golang v1.11.0 // indirect
-	github.com/oschwald/maxminddb-golang v1.13.0 // indirect
-)
+require github.com/oschwald/maxminddb-golang v1.13.0 // indirect
--- internal/nfqueue/callback.go
+++ internal/nfqueue/callback.go
@@ -1,110 +1,125 @@
 package nfqueue
 
 import (
-	"encoding/binary"
-	"fmt"
-	"net"
+    "encoding/binary"
+    "fmt"
+    "net"
 
-	"github.com/chifflier/nfqueue-go/nfqueue"
-  "github.com/lonelysadness/netmonitor/internal/geoip"
-	"github.com/lonelysadness/netmonitor/pkg/utils"
-	"golang.org/x/sys/unix"
+    "github.com/chifflier/nfqueue-go/nfqueue"
+    "github.com/lonelysadness/netmonitor/internal/geoip"
+    "github.com/lonelysadness/netmonitor/pkg/utils"
+    "golang.org/x/sys/unix"
 )
 
 func handleIPv4(packet []byte) (net.IP, net.IP, uint8) {
-	ipHeader := packet[:20]
-	srcIP := net.IP(ipHeader[12:16])
-	dstIP := net.IP(ipHeader[16:20])
-	protocol := ipHeader[9]
-	return srcIP, dstIP, protocol
+    ipHeader := packet[:20]
+    srcIP := net.IP(ipHeader[12:16])
+    dstIP := net.IP(ipHeader[16:20])
+    protocol := ipHeader[9]
+    return srcIP, dstIP, protocol
 }
 
 func handleIPv6(packet []byte) (net.IP, net.IP, uint8) {
-	ipHeader := packet[:40]
-	srcIP := net.IP(ipHeader[8:24])
-	dstIP := net.IP(ipHeader[24:40])
-	protocol := ipHeader[6]
-	return srcIP, dstIP, protocol
+    ipHeader := packet[:40]
+    srcIP := net.IP(ipHeader[8:24])
+    dstIP := net.IP(ipHeader[24:40])
+    protocol := ipHeader[6]
+    return srcIP, dstIP, protocol
 }
 
 func handleTCP(packet []byte, headerLength int) {
-	tcpHeader := packet[headerLength : headerLength+20]
-	srcPort := binary.BigEndian.Uint16(tcpHeader[0:2])
-	dstPort := binary.BigEndian.Uint16(tcpHeader[2:4])
-	fmt.Printf("Source Port: %d, Destination Port: %d", srcPort, dstPort)
+    tcpHeader := packet[headerLength : headerLength+20]
+    srcPort := binary.BigEndian.Uint16(tcpHeader[0:2])
+    dstPort := binary.BigEndian.Uint16(tcpHeader[2:4])
+    fmt.Printf("Source Port: %d, Destination Port: %d", srcPort, dstPort)
 }
 
 func handleUDP(packet []byte, headerLength int) {
-	udpHeader := packet[headerLength : headerLength+8]
-	srcPort := binary.BigEndian.Uint16(udpHeader[0:2])
-	dstPort := binary.BigEndian.Uint16(udpHeader[2:4])
-	fmt.Printf("Source Port: %d, Destination Port: %d", srcPort, dstPort)
+    udpHeader := packet[headerLength : headerLength+8]
+    srcPort := binary.BigEndian.Uint16(udpHeader[0:2])
+    dstPort := binary.BigEndian.Uint16(udpHeader[2:4])
+    fmt.Printf("Source Port: %d, Destination Port: %d", srcPort, dstPort)
 }
 
 func handleICMP(packet []byte, headerLength int) {
-	icmpHeader := packet[headerLength : headerLength+4]
-	icmpType := icmpHeader[0]
-	icmpCode := icmpHeader[1]
-	fmt.Printf("ICMP Type: %d, ICMP Code: %d", icmpType, icmpCode)
+    icmpHeader := packet[headerLength : headerLength+4]
+    icmpType := icmpHeader[0]
+    icmpCode := icmpHeader[1]
+    fmt.Printf("ICMP Type: %d, ICMP Code: %d", icmpType, icmpCode)
 }
 
 func handleICMPv6(packet []byte, headerLength int) {
-	icmpHeader := packet[headerLength : headerLength+4]
-	icmpType := icmpHeader[0]
-	icmpCode := icmpHeader[1]
-	fmt.Printf("ICMPv6 Type: %d, ICMPv6 Code: %d", icmpType, icmpCode)
+    icmpHeader := packet[headerLength : headerLength+4]
+    icmpType := icmpHeader[0]
+    icmpCode := icmpHeader[1]
+    fmt.Printf("ICMPv6 Type: %d, ICMPv6 Code: %d", icmpType, icmpCode)
 }
 
 func Callback(payload *nfqueue.Payload) int {
-	packet := payload.Data
-	var srcIP, dstIP net.IP
-	var protocol uint8
+    packet := payload.Data
+    var srcIP, dstIP net.IP
+    var protocol uint8
 
-	// Determine if it's IPv4 or IPv6
-	switch packet[0] >> 4 {
-	case 4:
-		srcIP, dstIP, protocol = handleIPv4(packet)
-	case 6:
-		srcIP, dstIP, protocol = handleIPv6(packet)
-	default:
-		fmt.Println("Unknown IP version")
-		payload.SetVerdict(nfqueue.NF_ACCEPT)
-		return 0
-	}
-  
-  srcCountry := geoip.LookupCountry(srcIP)
-  dstCountry := geoip.LookupCountry(dstIP)
+    // Determine if it's IPv4 or IPv6
+    switch packet[0] >> 4 {
+    case 4:
+        srcIP, dstIP, protocol = handleIPv4(packet)
+    case 6:
+        srcIP, dstIP, protocol = handleIPv6(packet)
+    default:
+        fmt.Println("Unknown IP version")
+        payload.SetVerdict(nfqueue.NF_ACCEPT)
+        return 0
+    }
 
-	fmt.Printf("Source IP: %s (%s), Destination IP: %s (%s), Protocol: %s, ", srcIP, srcCountry, dstIP, dstCountry, utils.GetProtocolName(protocol))
+    srcCountry := geoip.LookupCountry(srcIP)
+    dstCountry := geoip.LookupCountry(dstIP)
 
-	headerLength := 0
-	if (packet[0] >> 4) == 4 {
-		headerLength = int(packet[0]&0x0F) * 4
-	} else if (packet[0] >> 4) == 6 {
-		headerLength = 40
-	}
+    // Print source IP and country if applicable
+    if srcCountry != "" {
+        fmt.Printf("Source IP: %s (%s)", srcIP, srcCountry)
+    } else {
+        fmt.Printf("Source IP: %s", srcIP)
+    }
 
-	switch protocol {
-	case unix.IPPROTO_TCP:
-		if len(packet) >= headerLength+20 {
-			handleTCP(packet, headerLength)
-		}
-	case unix.IPPROTO_UDP:
-		if len(packet) >= headerLength+8 {
-			handleUDP(packet, headerLength)
-		}
-	case unix.IPPROTO_ICMP:
-		if len(packet) >= headerLength+4 {
-			handleICMP(packet, headerLength)
-		}
-	case unix.IPPROTO_ICMPV6:
-		if len(packet) >= headerLength+4 {
-			handleICMPv6(packet, headerLength)
-		}
-	}
+    // Print destination IP and country if applicable
+    if dstCountry != "" {
+        fmt.Printf(", Destination IP: %s (%s)", dstIP, dstCountry)
+    } else {
+        fmt.Printf(", Destination IP: %s", dstIP)
+    }
 
-	fmt.Println()
-	payload.SetVerdict(nfqueue.NF_ACCEPT)
-	return 0
+    // Print protocol
+    fmt.Printf(", Protocol: %s, ", utils.GetProtocolName(protocol))
+
+    headerLength := 0
+    if (packet[0] >> 4) == 4 {
+        headerLength = int(packet[0]&0x0F) * 4
+    } else if (packet[0] >> 4) == 6 {
+        headerLength = 40
+    }
+
+    switch protocol {
+    case unix.IPPROTO_TCP:
+        if len(packet) >= headerLength+20 {
+            handleTCP(packet, headerLength)
+        }
+    case unix.IPPROTO_UDP:
+        if len(packet) >= headerLength+8 {
+            handleUDP(packet, headerLength)
+        }
+    case unix.IPPROTO_ICMP:
+        if len(packet) >= headerLength+4 {
+            handleICMP(packet, headerLength)
+        }
+    case unix.IPPROTO_ICMPV6:
+        if len(packet) >= headerLength+4 {
+            handleICMPv6(packet, headerLength)
+        }
+    }
+
+    fmt.Println()
+    payload.SetVerdict(nfqueue.NF_ACCEPT)
+    return 0
 }
 
